845ebcb85544db635f4742ab77c4367d5e2c568e
Bartosz Krych <bartosz.krych@synapse.com>
2019-08-09 16:13:13 +0200

    Add getting data in *.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TimePreq } from './model/time-preq';
+import { TimePreqService } from './service/time-preq/time-preq.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit  {
+
+  times : TimePreq[];
+  
+  constructor(private timePreqService: TimePreqService){}
+
+  ngOnInit(): void {
+    this.reloadData()
+  }
+
+  reloadData(){
+    this.timePreqService.getAllTimesPreq().subscribe(data => {
+      this.times = data;
+    });
+  }
+
 }
