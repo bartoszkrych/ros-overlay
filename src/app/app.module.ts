335806b8b1ad34b0ec9839ccc030086ba806fe59
Bartosz Krych <bartosz.krych@synapse.com>
2019-08-12 16:18:24 +0200

    carousel - ngb

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 
 import { AppComponent } from './app.component';
@@ -13,7 +14,8 @@ import { TimePreqService } from './service/time-preq/time-preq.service';
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    NgbModule,
   ],
   providers: [TimePreqService],
   bootstrap: [AppComponent]
