cf802af2ff60356eb02f7b256fc66bcbf3eabed2
Bartosz Krych <bartosz.krych@globallogic.com>
2019-09-03 13:28:23 +0200

    Add tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,14 +1,23 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { TimePreqService } from './service/time-preq/time-preq.service';
+import { HttpClientModule } from '@angular/common/http';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 describe('AppComponent', () => {
+  let service: TimePreqService;
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
         AppComponent,
       ],
+      imports: [HttpClientModule, NgbModule],
+      providers: [TimePreqService]
     }).compileComponents();
+    service = TestBed.get(TimePreqService);
   }));
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
 });
--- src/app/service/time-preq/time-preq.service.spec.ts
+++ src/app/service/time-preq/time-preq.service.spec.ts
@@ -1,11 +1,25 @@
-import { TestBed, inject } from '@angular/core/testing';
-
+import { TestBed } from '@angular/core/testing';
 import { TimePreqService } from './time-preq.service';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('TimePreqService', () => {
+  let service: TimePreqService;
+
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [HttpClientModule],
       providers: [TimePreqService]
     });
+    service = TestBed.get(TimePreqService);
   });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+  it('should returned list of cars', () => {
+    service.getAllTimesPreq().subscribe(data => {
+      expect(data.length).toBeGreaterThan(0);
+    });
+  })
 });
